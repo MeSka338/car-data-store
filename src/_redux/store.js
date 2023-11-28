0c26b07a83126500d2270bf1fdce3e6be3c70fa2
MeSka338 <halk49114@gmail.com>
2023-11-28 18:42:45 +0300

    Change store: import reducer. #55

--- src/_redux/store.js
+++ src/_redux/store.js
@@ -2,7 +2,8 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./car/sagas";
-import reduser from "./index";
+// import reduser from "./index";
+import { reduser } from ".";
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
